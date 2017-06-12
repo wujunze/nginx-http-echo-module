8f8c5aa88ba1391a10e1e626705135db6ff212e2
wujunze <itwujunze@163.com>
2017-06-12 19:06:51 +0800

    定义模块配置结构体 ngx_http_echo_loc_conf_t

---
+++ src/nginx_moudle_echo.c
@@ -0,0 +1,6 @@
+//
+// Created by wujunze on 2017/6/12.
+//
+typedef struct {
+    ngx_str_t ed;  //该结构体定义在这里 https://github.com/nginx/nginx/blob/master/src/core/ngx_string.h
+} ngx_http_echo_loc_conf_t;
